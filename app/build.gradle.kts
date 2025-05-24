07da8adda0012863e48fdf7993006d85080f767d
root <root@WIN-U5ESP2I9R8V.>
2025-05-24 19:40:53 +0300

    first

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+	application
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStandardStreams = true
+	}
+}
