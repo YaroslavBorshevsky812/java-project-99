46e598b65e0b9d1f3d32a760beefbb8dbd0f5076
root <root@WIN-U5ESP2I9R8V.>
2025-05-24 20:35:53 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,18 @@ plugins {
 	application
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
+	jacoco
+	id("checkstyle")
+	id("io.freefair.lombok") version "8.13.1"
+	id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "YaroslavBorshevsky812_java-project-99")
+		property("sonar.organization", "yaroslavborshevsky812")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 
@@ -25,11 +37,23 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+
 tasks.test {
 	useJUnitPlatform()
 	testLogging {
 		exceptionFormat = TestExceptionFormat.FULL
 		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStackTraces = true
+		showCauses = true
 		showStandardStreams = true
+	}
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
 	}
 }
