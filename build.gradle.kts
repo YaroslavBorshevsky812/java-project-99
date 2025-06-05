cd080bbc2ad9eb4801918c5cc245d972c6f9e0f6
root <root@WIN-U5ESP2I9R8V.localdomain>
2025-06-05 17:15:17 +0300

    build gradle fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-	java
+	application
+	jacoco
+	id("checkstyle")
+	id("io.freefair.lombok") version "8.13.1"
+	id("org.sonarqube") version "4.4.1.3373"
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -13,8 +20,18 @@ java {
 	}
 }
 
+application { mainClass.set("hexlet.code.AppApplication") }
+
 repositories {
 	mavenCentral()
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "YaroslavBorshevsky812_java-project-99")
+		property("sonar.organization", "yaroslavborshevsky812")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 dependencies {
@@ -23,6 +40,22 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<Test> {
+tasks.test {
 	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStackTraces = true
+		showCauses = true
+		showStandardStreams = true
+	}
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+	}
 }
