6c36b9dcb9b73bf8c9701b1d41dac90f5e9d4a78
root <root@WIN-U5ESP2I9R8V.localdomain>
2025-06-24 18:18:01 +0300

    mapper fix

--- src/main/java/hexlet/code/mapper/StatusMapper.java
+++ src/main/java/hexlet/code/mapper/StatusMapper.java
@@ -4,10 +4,18 @@ import hexlet.code.dto.status.StatusCreateDTO;
 import hexlet.code.dto.status.StatusDTO;
 import hexlet.code.dto.status.StatusUpdateDTO;
 import hexlet.code.model.Status;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
-import org.springframework.stereotype.Component;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
-@Component
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    uses = {JsonNullableMapper.class}
+)
 public abstract class StatusMapper {
     public abstract StatusDTO toDto(Status entity);
 
